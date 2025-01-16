cdecd01c4a4049367dfcaffe26995d6d77245ca4
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-01-16 16:42:28 +0530

    changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home__container">
+      <h2>Welcome to our Generative Album</h2>
+      <button>Get started</button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
