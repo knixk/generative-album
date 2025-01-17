195f2ac9f8d742790609da756c7d885e977fca9e
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-01-17 13:46:43 +0530

    added more data

--- frontend/src/pages/Album.jsx
+++ frontend/src/pages/Album.jsx
@@ -24,6 +24,26 @@ const data = [
     img: "https://cdn.pixabay.com/photo/2023/01/26/22/13/ai-generated-7747149_1280.jpg",
     name: "Saniya Sultana",
   },
+  {
+    id: "12312312",
+    img: "https://img.freepik.com/premium-photo/generative-ai-image-victoria-majestic-waterfall-with-rock-stone-cliff-nature-rainbow-wallpaper_467541-15025.jpg",
+    name: "Kavya Maurya",
+  },
+  {
+    id: "12312312",
+    img: "https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://fdczvxmwwjwpwbeeqcth.supabase.co/storage/v1/object/public/images/268c4b03-533f-4e81-aec1-0c22df466a90/2e6a3dd3-e59f-445e-b998-1c03afd15b3e.png",
+    name: "Ajay Sisodiya",
+  },
+  {
+    id: "12312312",
+    img: "https://as2.ftcdn.net/jpg/05/36/62/97/1000_F_536629702_9FkGvzElExv4TpJwr5S03yJHdS3hADR6.jpg",
+    name: "Arun Gehlot",
+  },
+  {
+    id: "12312312",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ69EWR5a90jYQWOYu8vdbbzLasqQNWHhyVnA&s",
+    name: "Priyanka Sharma",
+  },
 ];
 
 function Album() {
